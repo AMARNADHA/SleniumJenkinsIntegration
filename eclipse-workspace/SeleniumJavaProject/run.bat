74abd9172efc3809ad05f10542d2435ae7b81946
AMARNADHA <amarnatha@yahoo.com>
2020-06-01 12:17:33 +0530

    new commit

--- eclipse-workspace/SeleniumJavaProject/run.bat
+++ eclipse-workspace/SeleniumJavaProject/run.bat
@@ -1,1 +1,17 @@
 java -cp bin;lib/* org.testing.TestNg testng.xml
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+
+ public void testjenkins() {
+
+System.out.println("Selenium Jenkins Test");
+
+}
+
+------------------
+Path of Selenium demo project
+
+C:\Users\apasupuleti\eclipse-workspace\demoselenium
